b47a2dd25e559baaaae3abf6a70262275e99cfdd
Viktor <pritulyukv@gmail.com>
2020-11-07 23:59:36 +0200

    fix change BackGround

--- lesson_js_3_Viktor_Pritulyuk/js/color.js
+++ lesson_js_3_Viktor_Pritulyuk/js/color.js
@@ -2,9 +2,14 @@ document.addEventListener('DOMContentLoaded', function (event) {
   const pressTitle = document.querySelector('.press-title'),
     nameColor = document.querySelector('.name-color'),
     nameColorHex = document.querySelector('.name-color-hex');
+  let keyUp = 1;
 
   document.addEventListener('keydown', function (event) {
-    if (event.code == 'Space') changeBackgroundColor();
+    if (event.code == 'Space' && keyUp) changeBackgroundColor();
+  });
+
+  document.addEventListener('keyup', function (event) {
+    if (event.code == 'Space') keyUp = 1;
   });
 
   function getRandom() {
@@ -33,5 +38,6 @@ document.addEventListener('DOMContentLoaded', function (event) {
     } else {
       pressTitle.style.color = nameColor.style.color = nameColorHex.style.color = 'black';
     }
+    keyUp = 0;
   };
 });
