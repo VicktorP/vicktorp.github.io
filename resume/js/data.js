414e315dd2da7bf4c75fc5c739a3457e59397929
Viktor <pritulyukv@gmail.com>
2021-08-10 20:22:31 +0300

    10.08 20:22

--- resume/js/data.js
+++ resume/js/data.js
@@ -1,100 +1,205 @@
 const data = {
-  RU: {
-    titleMyself: "О себе",
-    myself: "Спокоен, прагматичен, целеустремлен",
-    stress: "Стрессоустойчивость",
-    teamWork: "Работа в коллективе",
-    english: "Английский язык - В2",
-    google: "Умение гуглить",
-    fullname: "Притулюк Виктор",
+  UKR: {
+    stress: "Стрессостійкість",
+    sociability: "Комунікабельність",
+    organization: "Організованість",
+    skillLanguages: "Мови",
+    languageUkr: "Українська",
+    languageRus: "Російська",
+    languageEng: "Англійська",
+    fullname: "Притулюк Віктор",
+    myMission: `Добридень. <br /> 
+                Моя мета - знайти нову можливість для зростання як фахівця. Я практикую HTML5, CSS3, JavaScript. В даний момент вивчаю React. Перебуваю зараз в 
+                передмісті Києва, але до міста або області не прив'язаний і готовий до релокації. Для мене важлива робота на результат в команді.`,
+    myExperience: "Досвід роботи",
+    myEducation: "Освіта",
     experience: [
       {
-        company: "не работаю",
-        about: "В связи с карантином моя должность была сокращена и я был уволен по соглашению сторон."
+        company: "Фріланс",
+        position: "Frontend розробник trainee",
+        date: "07.2020 - сьогодення",
+        duties: [
+          "Під керівництвом колег менторів займаюсь розробкою і створенням веб додатків",
+          "Оптимізую, обновлюю та вношу зміни у застарілі сайти",
+          "Продовжую самонавчання програмуванню"
+        ]
       }, {
-        company: "компания SanWELL",
-        about:  "Компания занимается поставками сантехнического оборудования. За время работы в компании занимал должности: зав.отделом склада, продавец-консультант, на момент увольнения работал в контент отделе фотографом."
+        company: "SanWell - оптова торгівля, дистрибуція, імпорт та експорт сантехнічної продукції",
+        position: "Менеджер відділу маркетингу",
+        date: "10.2017 - 07.2020",
+        duties: [
+          "Контроль наповнення інтернет-магазину компанії актуальною продукцією",
+          "Планування і розробка рекламних акцій",
+          "Пошук / створення / редагування контенту"
+        ]
       }, {
-        company: "ЧП Плаушевский",
-        about:  'Компания занималась поставками пива "Славутич". Работал там торговым представителем'
+        company: "SanWell - оптова торгівля, дистрибуція, імпорт та експорт сантехнічної продукції",
+        position: "Завідуючий роздрібним відділом складу",
+        date: "05.2012 - 10.2017",
+        duties: [
+          "Забезпечення безперебійної роботи складу",
+          "Ведення складського обліку",
+          "Управління колективом, розподіл задач"
+        ]
       }
     ],
     education: [
       {
-        school: 'IT SCHOOL "Peace IT"',
-        about: 'Прошел с отличием курс обучения по специальности "Front-end разработчик"'
+        company: 'IT School "Peace IT"',
+        position: '"Frontend розробник"',
+        date: "09.2020 - 04.2021",
+        description: "Закінчив з відзнакою"
       }, {
-        school: 'Компьютерная академия "ШАГ"',
-        about: 'Прошел курс обучения по специальности "Web разработка"'
+        company: `Комп'ютерна академія "КРОК"`,
+        position: '"Web розробка"',
+        date: "04.2018 - 04.2019",
+        description: ""
       }, {
-        school: "ВНУ им. В.Даля",
-        about: 'На очном отделении получил специальность "Автоматика и системы управления", квалификация специалист'
+        company: 'Інститут профессіональних технологій',
+        position: `"Технічне обслуговування персонального комп'ютера"`,
+        date: "01.2018 - 03.2018",
+        description: ""
+      }, {
+        company: 'Східноукраїнський Національный університет ім. Володимира Даля',
+        position: '"Автоматика та системи управління"',
+        date: "09.2000 - 06.2005",
+        description: "кваліфікація - спеціаліст"
+      }
+    ]
+  },
+  RU: {
+    stress: "Стрессоустойчивость",
+    sociability: "Коммуникабельность",
+    organization: "Организованность",
+    skillLanguages: "Языки",
+    languageUkr: "Украинский",
+    languageRus: "Русский",
+    languageEng: "Английский",
+    fullname: "Притулюк Виктор",
+    myMission: `Здравствуйте. <br /> 
+              Моя цель - найти новую возможность для роста как специалиста. Я практикую HTML5, CSS3, JavaScript. В данный момент изучаю React. Проживаю в пригороде Киева,
+              но к городу или области не привязан и готов к релокации. Для меня важна работа на результат в команде.`,
+    myExperience: "Опыт работы",
+    myEducation: "Образование",
+    experience: [
+      {
+        company: "Фриланс",
+        position: "Frontend разработчик trainee",
+        date: "07.2020 - настоящее время",
+        duties: [
+          "Под руководством коллег менторов занимаюсь разработкой и созданием веб приложений",
+          "Оптимизирую, обновляю и вношу изменения в устаревшие сайты",
+          "Самообучаюсь программированию"
+        ]
+      }, {
+        company: "SanWell - оптовая торговля, дистрибуция, импорт и экспорт сантехнической продукции",
+        position: "Менеджер отдела маркетинга",
+        date: "10.2017 - 07.2020",
+        duties: [
+          "Контроль наполнения интернет-магазина компании актуальной продукцией",
+          "Планирование и разработка рекламных акций",
+          "Поиск / создание / редактирование контента"
+        ]
+      }, {
+        company: "SanWell - оптовая торговля, дистрибуция, импорт и экспорт сантехнической продукции",
+        position: "Заведующий розничным отделом склада",
+        date: "05.2012 - 10.2017",
+        duties: [
+          "Обеспечение бесперебойной работы склада",
+          "Ведение складского учета",
+          "Управление коллективом, распределение задач"
+        ]
+      }
+    ],
+    education: [
+      {
+        company: 'IT School "Peace IT"',
+        position: '"Frontend разработчик"',
+        date: "09.2020 - 04.2021",
+        description: "Закончил с отличием"
+      }, {
+        company: 'Компьютерная академия "ШАГ"',
+        position: '"Web разработка"',
+        date: "04.2018 - 04.2019",
+        description: ""
+      }, {
+        company: 'Институт профессиональных технологий',
+        position: '"Техническое обслуживание персонального компьютера"',
+        date: "01.2018 - 03.2018",
+        description: ""
+      }, {
+        company: 'Восточноукраинский Национальный университет им. Владимира Даля',
+        position: '"Автоматика и системы управления"',
+        date: "09.2000 - 06.2005",
+        description: "квалификация - специалист"
       }
     ]
   },
   ENG: {
-    titleMyself: "About myself",
-    myself: "Calm, pragmatic, purposeful",
     stress: "Stress tolerance",
-    teamWork: "Teamwork",
-    english: "English - B2",
-    google: "Ability to google",
+    sociability: "Sociability",
+    organization: "Organization",
+    skillLanguages: "Languages",
+    languageUkr: "Ukrainian",
+    languageRus: "Russian",
+    languageEng: "English",
     fullname: "Viktor Pritulyuk",
+    myMission: `Hello. <br /> 
+                My goal is to find a new opportunity for growth as a specialist. I practice HTML5, CSS3, JavaScript. I am currently learning React. I live in the suburbs 
+                of Kiev, but I am not tied to the city or region and is ready for relocation. Working for results in a team is important for me.`,
+    myExperience: "Work experience",
+    myEducation: "Education",
     experience: [
       {
-        company: "don't work",
-        about: "Due to quarantine, my position was reduced and I was dismissed by agreement of the parties."
+        company: "Freelance",
+        position: "Frontend developer trainee",
+        date: "07.2020 - present time",
+        duties: [
+          "Under the guidance of fellow mentors, I am engaged in the development and creation of web applications",
+          "Optimizing, updating and making changes to outdated sites",
+          "Self-study programming"
+        ]
       }, {
-        company: "SanWELL company",
-        about:  "The company is engaged in the supply of sanitary equipment. During my work in the company I held positions: head of the warehouse department, sales assistant, at the time of my dismissal I worked in the content department as a photographer."
+        company: "SanWell - wholesale, distribution, import and export of sanitary products",
+        position: "Marketing manager",
+        date: "10.2017 - 07.2020",
+        duties: [
+          "Control of filling the company's online store with actual products",
+          "Planning and development of promotions",
+          "Search / create / edit content"
+        ]
       }, {
-        company: "PE Plaushevsky",
-        about:  'The company was engaged in the supply of beer "Slavutich". I worked there as a sales manager.'
+        company: "SanWell - wholesale, distribution, import and export of sanitary products",
+        position: "Head of the retail department of the warehouse",
+        date: "05.2012 - 10.2017",
+        duties: [
+          "Ensuring the smooth operation of the warehouse",
+          "Warehouse accounting",
+          "Team management, distribution of tasks"
+        ]
       }
     ],
     education: [
       {
-        school: 'IT SCHOOL "Peace IT"',
-        about: 'Completed a training course with honors in the specialty "Front-end developer".'
+        company: 'IT School "Peace IT"',
+        position: '"Frontend developer"',
+        date: "09.2020 - 04.2021",
+        description: "Graduated with honors"
       }, {
-        school: 'Computer Academy "STEP"',
-        about: 'Completed a course in the specialty "Web Development".'
+        company: 'Computer Academy "STEP"',
+        position: '"Web development"',
+        date: "04.2018 - 04.2019",
+        description: ""
       }, {
-        school: "ENU V.Dahl",
-        about: 'In the full-time department I received the specialty "Automation and control systems", qualification specialist.'
-      }
-    ]
-  },
-  UKR: {
-    titleMyself: "Про мене",
-    myself: "Спокійний, прагматичний, цілеспрямований",
-    stress: "Cтресостійкість",
-    teamWork: "Робота в колективі",
-    english: "Англійська мова - В2",
-    google: "Вміння гуглити",
-    fullname: "Притулюк Вiктор",
-    experience: [
-      {
-        company: "не працюю",
-        about: "У зв'язку з карантином моя посада була скорочена і я був звільнений за згодою сторін."
+        company: 'Institute of Professional Technologies',
+        position: '"Maintenance of a personal computer"',
+        date: "01.2018 - 03.2018",
+        description: ""
       }, {
-        company: "компанія SanWELL",
-        about:  "Компанія займається поставками сантехнічного обладнання. За час роботи в компанії я займав посади: зав.відділом складу, продавець-консультант, на момент звільнення працював в контент відділі фотографом."
-      }, {
-        company: "ПП Плаушевський",
-        about:  'Компанія займалася поставками пива "Славутич". Я працював там торговим представником'
-      }
-    ],
-    education: [
-      {
-        school: 'IT SCHOOL "Peace IT"',
-        about: 'Пройшов з відзнакою курс навчання за спеціальністю "Front-end розробник".'
-      }, {
-        school: 'Комп\'ютерна академія "ШАГ"',
-        about: 'Пройшов курс навчання за спеціальністю "Web розробка".'
-      }, {
-        school: "СНУ ім. В.Даля",
-        about: 'На очному відділенні отримав спеціальність "Автоматика і системи управління", кваліфікація спеціаліст.'
+        company: 'East Ukrainian National University. Vladimir Dahl',
+        position: '"Automation and control systems"',
+        date: "09.2000 - 06.2005",
+        description: "qualification - specialist"
       }
     ]
   }
--- resume/js/script.js
+++ resume/js/script.js
@@ -1,31 +1,77 @@
 document.addEventListener("DOMContentLoaded", function () {
+  const cvvData           = document.querySelector("#cvv-data");
   const langShower        = document.querySelector("#language-shower");
   const langDownArrow     = document.querySelector("#language-dropdown-arrow");
   const langCheckWrapper  = document.querySelector("#languages-checker-wrapper");
   const langItems         = document.querySelectorAll(".js-language-select");
-  const titleMyself       = document.querySelector("#skill-title-myself");
-  const skillMyself       = document.querySelector("#skill-myself");
-  const skillStress       = document.querySelector("#skill-stress");
-  const skillTeamWork     = document.querySelector("#skill-team-work");
-  const skillEnglish      = document.querySelector("#skill-english");
-  const skillGoogle       = document.querySelector("#skill-google");
-  const fullName          = document.querySelector("#my-full-name");
-  const lastWork          = document.querySelector("#last-work");
-  const lastWorkDesc      = document.querySelector("#last-work-description");
-  const middleWork        = document.querySelector("#middle-work");
-  const middleWorkDesc    = document.querySelector("#middle-work-description");
-  const firstWork         = document.querySelector("#first-work");
-  const firstWorkDesc     = document.querySelector("#first-work-description");
-  const lastStudy         = document.querySelector("#last-study");
-  const lastStudyDesc     = document.querySelector("#last-study-description");
-  const middleStudy       = document.querySelector("#middle-study");
-  const middleStudyDesc   = document.querySelector("#middle-study-description");
-  const firstStudy        = document.querySelector("#first-study");
-  const firstStudyDesc    = document.querySelector("#first-study-description");
+  const stress            = document.querySelector("#skill-stress");
+  const sociability       = document.querySelector("#skill-sociability");
+  const organization      = document.querySelector("#skill-organization");
+  const titleLanguages    = document.querySelector("#skill-title-languages");
+  const languageUkr       = document.querySelector("#skill-language-ukr");
+  const languageRus       = document.querySelector("#skill-language-rus");
+  const languageEng       = document.querySelector("#skill-language-eng");
 
   
-  let selectedLanguage = "RU";
+  
+  let selectedLanguage = "ENG";
   let degree = 0;
+
+  const renderStageInfo = (stage) => {
+    let stageHTML = "";
+    for (let i = 0; i < stage.length; i++) {
+      stageHTML += `
+        <div class="stage-info">
+          <h3 class="job-title">${stage[i].position}</h3>
+          <p class="stage-date">${stage[i].date}</p>
+          <p class="stage-place">${stage[i].company}</p>
+        `;
+      if (stage[i].duties) {
+        stageHTML += `
+              <ul class="job-duties">
+              <li class="duty-item">${stage[i].duties[0]}</li>
+              <li class="duty-item">${stage[i].duties[1]}</li>
+              <li class="duty-item">${stage[i].duties[2]}</li>
+            </ul>
+          </div>
+        `;
+      } else if (stage[i].description) {
+        stageHTML += `
+            <p class="study-description">${stage[i].description}</p>
+          </div>
+        `;
+      }    
+    }
+    return stageHTML;
+  }
+
+  const renderCVV = (lang) => {
+    stress.innerText = data[lang].stress;
+    sociability.innerText = data[lang].sociability;
+    organization.innerText = data[lang].organization;
+    titleLanguages.innerText = data[lang].skillLanguages;
+    languageUkr.innerText = data[lang].languageUkr;
+    languageRus.innerText = data[lang].languageRus;
+    languageEng.innerText = data[lang].languageEng;
+
+    cvvData.innerHTML = `
+      <section class="section-title">
+        <h1 class="my-title">${data[lang].fullname}</h1>
+        <p class="job-title">Front-end web developer junior</p>
+        <p class="my-mission">${data[lang].myMission}</p>
+      </section>
+      <section class="section-stage">
+        <h2 class="stage-title">${data[lang].myExperience}</h2>
+        ${renderStageInfo(data[lang].experience)}
+      </section>
+      <section class="section-stage">
+        <h2 class="stage-title">${data[lang].myEducation}</h2>
+        ${renderStageInfo(data[lang].education)}
+      </section>
+    `;
+  }
+
+  renderCVV(selectedLanguage);
 
   const changeShowLanguagesList = () => {
     langCheckWrapper.classList.toggle("js-over");
@@ -33,6 +79,7 @@ document.addEventListener("DOMContentLoaded", function () {
     langDownArrow.style.transform = `rotate(${degree}deg)`;
   }
 
+ 
   const replaceLanguage = (event) => {
     selectedLanguage = event.target.innerText; 
     langShower.innerText = selectedLanguage;
@@ -47,27 +94,6 @@ document.addEventListener("DOMContentLoaded", function () {
   langDownArrow.addEventListener("click", changeShowLanguagesList);
 
   langShower.addEventListener("click", changeShowLanguagesList);
-  
-  const renderCVV = (lang) => {
-    titleMyself.textContent     = data[lang].titleMyself;
-    skillMyself.textContent     = data[lang].myself;
-    skillStress.textContent     = data[lang].stress;
-    skillTeamWork.textContent   = data[lang].teamWork;
-    skillEnglish.textContent    = data[lang].english;
-    skillGoogle.textContent     = data[lang].google;
-    fullName.textContent        = data[lang].fullname;
-    lastWork.textContent        = data[lang].experience[0].company;
-    lastWorkDesc.textContent    = data[lang].experience[0].about;
-    middleWork.textContent      = data[lang].experience[1].company;
-    middleWorkDesc.textContent  = data[lang].experience[1].about;
-    firstWork.textContent       = data[lang].experience[2].company;
-    firstWorkDesc.textContent   = data[lang].experience[2].about;
-    lastStudy.textContent       = data[lang].education[0].school;
-    lastStudyDesc.textContent   = data[lang].education[0].about;
-    middleStudy.textContent     = data[lang].education[1].school;
-    middleStudyDesc.textContent = data[lang].education[1].about;
-    firstStudy.textContent      = data[lang].education[2].school;
-    firstStudyDesc.textContent  = data[lang].education[2].about;
-  }
+
 
 });
