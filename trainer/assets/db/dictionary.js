cc58664ce97bda0eb28087c4357092257d161f6d
Viktor <myemail@gmail.com>
2023-05-08 01:50:26 +0200

    word master

---
+++ trainer/assets/db/dictionary.js
@@ -0,0 +1,537 @@
+const wordsList = [
+    {
+        word: "to abuse",
+        translation: "злоупотреблять, оскорблять, плохо обращаться, браниться",
+        transcription: "ə'bju:s",
+        rate: 1,
+        success: 0,
+        statistics: []
+    }, {
+        word: "to keep up",
+        translation: "поддерживать, успевать",
+        transcription: "kēp əp",
+        rate: 1,
+        success: 0,
+        statistics: []
+    }, {
+        word: "roof",
+        translation: "крыша",
+        transcription: "ro͞of",
+        rate: 1,
+        success: 0,
+        statistics: []
+    }, {
+        word: "relief",
+        translation: "облегчение",
+        transcription: "rə'lēf",
+        rate: 1,
+        success: 0,
+        statistics: []
+    }, {
+        word: "to pick up",
+        translation: "собирать (вещи), подобрать, забрать",
+        transcription: "pik up",
+        rate: 1,
+        success: 0,
+        statistics: []
+    }, {
+        word: "melting",
+        translation: "таяние, плавление, тающий",
+        transcription: "'meltiNG",
+        rate: 1,
+        success: 0,
+        statistics: []
+    }, {
+        word: "lovely",
+        translation: "красотка, прекрасный, красивый",
+        transcription: "'ləvlē",
+        rate: 1,
+        success: 0,
+        statistics: []
+    }, {
+        word: "lifespan",
+        translation: "длительность жизни",
+        transcription: "līfspan",
+        rate: 1,
+        success: 0,
+        statistics: []
+    }, {
+        word: "labour",
+        translation: "труд, работа, рабочие, трудиться",
+        transcription: "'leibər",
+        rate: 1,
+        success: 0,
+        statistics: []
+    }, {
+        word: "interrogative",
+        translation: "вопросительное слово, вопросительный",
+        transcription: ",in(t)ə'rägədiv",
+        rate: 1,
+        success: 0,
+        statistics: []
+    }, {
+        word: "exception",
+        translation: "исключение",
+        transcription: "ik'sepSH(ə)n",
+        rate: 1,
+        success: 0,
+        statistics: []
+    }, {
+        word: "despite",
+        translation: "несмотря на",
+        transcription: "də'spīt",
+        rate: 1,
+        success: 0,
+        statistics: []
+    }, {
+        word: "delicacy",
+        translation: "деликатность, деликатес, лакомство",
+        transcription: "'deləkəsē",
+        rate: 1,
+        success: 0,
+        statistics: []
+    }, {
+        word: "crucial",
+        translation: "решающий, ключевой",
+        transcription: "'kro͞oSHəl",
+        rate: 1,
+        success: 0,
+        statistics: []
+    }, {
+        word: "to consider",
+        translation: "рассматривать, учитывать, принимать во внимание",
+        transcription: "kən'sidər",
+        rate: 1,
+        success: 0,
+        statistics: []
+    }, {
+        word: "comprehension",
+        translation: "понимание, осмысление",
+        transcription: ",kämprə'hen(t)SH(ə)n",
+        rate: 1,
+        success: 0,
+        statistics: []
+    }, {
+        word: "can afford",
+        translation: "мочь позволить",
+        transcription: "kan ə'fôrd",
+        rate: 1,
+        success: 0,
+        statistics: []
+    }, {
+        word: "blank",
+        translation: "пробел, пустой, бланк, прикрывать",
+        transcription: "blaNGk",
+        rate: 1,
+        success: 0,
+        statistics: []
+    }, {
+        word: "bitter",
+        translation: "горький",
+        transcription: "'bidər",
+        rate: 1,
+        success: 0,
+        statistics: []
+    }, {
+        word: "because of",
+        translation: "из-за",
+        transcription: "bē'kəz əv",
+        rate: 1,
+        success: 0,
+        statistics: []
+    }, {
+        word: "average",
+        translation: "средний",
+        transcription: "'av(ə)rij",
+        rate: 1,
+        success: 0,
+        statistics: []
+    }, {
+        word: "affirmative",
+        translation: "утверждение, заявление, утвердительный, положительный",
+        transcription: "ə'fərmədiv",
+        rate: 1,
+        success: 0,
+        statistics: []
+    }, {
+        word: "to prolong",
+        translation: "продолжать",
+        transcription: "to͞o prə'lôNG",
+        rate: 1,
+        success: 0,
+        statistics: []
+    }, {
+        word: "to evolve",
+        translation: "развиваться, эволюционировать",
+        transcription: "to͞o ē'välv",
+        rate: 1,
+        success: 0,
+        statistics: []
+    }, {
+        word: "to depend on",
+        translation: "в зависимости от",
+        transcription: "to͞o də'pend on",
+        rate: 1,
+        success: 0,
+        statistics: []
+    }, {
+        word: "to come up",
+        translation: "придумать, придумывать",
+        transcription: "to͞o 'kəm ʌp",
+        rate: 1,
+        success: 0,
+        statistics: []
+    }, {
+        word: "to be forced",
+        translation: "быть вынужденным",
+        transcription: "to͞o bē fôrst",
+        rate: 1,
+        success: 0,
+        statistics: []
+    }, {
+        word: "to adapt",
+        translation: "адаптироваться",
+        transcription: "to͞o ə'dapt",
+        rate: 1,
+        success: 0,
+        statistics: []
+    }, {
+        word: "to take out",
+        translation: "выносить",
+        transcription: "tāk out",
+        rate: 1,
+        success: 0,
+        statistics: []
+    }, {
+        word: "surprisingly",
+        translation: "как ни странно, удивительно",
+        transcription: "sər'prīziNGlē",
+        rate: 1,
+        success: 0,
+        statistics: []
+    }, {
+        word: "significant",
+        translation: "значимый, значительный, важный",
+        transcription: "sig'nifikənt",
+        rate: 1,
+        success: 0,
+        statistics: []
+    }, {
+        word: "cabbage",
+        translation: "капуста",
+        transcription: "'kabij",
+        rate: 1,
+        success: 0,
+        statistics: []
+    }, {
+        word: "all over",
+        translation: "повсюду, полностью, кругом",
+        transcription: "ôl 'ōvər",
+        rate: 1,
+        success: 0,
+        statistics: []
+    }, {
+        word: "consonant",
+        translation: "согласная буква, согласный",
+        transcription: "'känsənənt",
+        rate: 1,
+        success: 0,
+        statistics: []
+    }, {
+        word: "vowel",
+        translation: "гласная буква, гласный",
+        transcription: "'vou(ə)l",
+        rate: 1,
+        success: 0,
+        statistics: []
+    }, {
+        word: "remediation",
+        translation: "исправление",
+        transcription: "rəˌmēdē'āSH(ə)n",
+        rate: 1,
+        success: 0,
+        statistics: []
+    }, {
+        word: "disturbance",
+        translation: "возмущение",
+        transcription: "də'stərbəns",
+        rate: 1,
+        success: 0,
+        statistics: []
+    }, {
+        word: "indignation",
+        translation: "возмущение",
+        transcription: "ˌindig'nāSH(ə)n",
+        rate: 1,
+        success: 0,
+        statistics: []
+    }, {
+        word: "to get along",
+        translation: "ладить (отшения между людьми)",
+        transcription: "",
+        rate: 1,
+        success: 0,
+        statistics: []
+    }, {
+        word: "to cross",
+        translation: "переходить",
+        transcription: "krôs",
+        rate: 1,
+        success: 0,
+        statistics: []
+    }, {
+        word: "terrified",
+        translation: "испуганный",
+        transcription: "",
+        rate: 1,
+        success: 0,
+        statistics: []
+    }, {
+        word: "annoyed",
+        translation: "раздраженный",
+        transcription: "ə'noid",
+        rate: 1,
+        success: 0,
+        statistics: []
+    }, {
+        word: "frustrated",
+        translation: "расстроенный",
+        transcription: "'frəˌstrādəd",
+        rate: 1,
+        success: 0,
+        statistics: []
+    }, {
+        word: "puzzled",
+        translation: "озадаченный",
+        transcription: "'pəz(ə)ld",
+        rate: 1,
+        success: 0,
+        statistics: []
+    }, {
+        word: "devious",
+        translation: "коварный",
+        transcription: "'dēvēəs",
+        rate: 1,
+        success: 0,
+        statistics: []
+    }, {
+        word: "embarrassed",
+        translation: "смущенный",
+        transcription: "əm'berəst",
+        rate: 1,
+        success: 0,
+        statistics: []
+    }, {
+        word: "obstinate",
+        translation: "упрямый",
+        transcription: "'äbstənət",
+        rate: 1,
+        success: 0,
+        statistics: []
+    }, {
+        word: "frightened",
+        translation: "испуганный",
+        transcription: "'frītnd",
+        rate: 1,
+        success: 0,
+        statistics: []
+    }, {
+        word: "shy",
+        translation: "застенчивый",
+        transcription: "SHī",
+        rate: 1,
+        success: 0,
+        statistics: []
+    }, {
+        word: "snobbish",
+        translation: "снобистский",
+        transcription: "'snäbiSH",
+        rate: 1,
+        success: 0,
+        statistics: []
+    }, {
+        word: "smug",
+        translation: "самодовольный",
+        transcription: "sməɡ",
+        rate: 1,
+        success: 0,
+        statistics: []
+    }, {
+        word: "lasting",
+        translation: "прочный, длительный",
+        transcription: "'lastiNG",
+        rate: 1,
+        success: 0,
+        statistics: []
+    }, {
+        word: "",
+        translation: "",
+        transcription: "",
+        rate: 1,
+        success: 0,
+        statistics: []
+    }, {
+        word: "",
+        translation: "",
+        transcription: "",
+        rate: 1,
+        success: 0,
+        statistics: []
+    }, {
+        word: "",
+        translation: "",
+        transcription: "",
+        rate: 1,
+        success: 0,
+        statistics: []
+    }, {
+        word: "",
+        translation: "",
+        transcription: "",
+        rate: 1,
+        success: 0,
+        statistics: []
+    }, {
+        word: "",
+        translation: "",
+        transcription: "",
+        rate: 1,
+        success: 0,
+        statistics: []
+    }, {
+        word: "",
+        translation: "",
+        transcription: "",
+        rate: 1,
+        success: 0,
+        statistics: []
+    }, {
+        word: "",
+        translation: "",
+        transcription: "",
+        rate: 1,
+        success: 0,
+        statistics: []
+    }, {
+        word: "",
+        translation: "",
+        transcription: "",
+        rate: 1,
+        success: 0,
+        statistics: []
+    }, {
+        word: "",
+        translation: "",
+        transcription: "",
+        rate: 1,
+        success: 0,
+        statistics: []
+    }, {
+        word: "",
+        translation: "",
+        transcription: "",
+        rate: 1,
+        success: 0,
+        statistics: []
+    }, {
+        word: "",
+        translation: "",
+        transcription: "",
+        rate: 1,
+        success: 0,
+        statistics: []
+    }, {
+        word: "",
+        translation: "",
+        transcription: "",
+        rate: 1,
+        success: 0,
+        statistics: []
+    }, {
+        word: "",
+        translation: "",
+        transcription: "",
+        rate: 1,
+        success: 0,
+        statistics: []
+    }, {
+        word: "",
+        translation: "",
+        transcription: "",
+        rate: 1,
+        success: 0,
+        statistics: []
+    }, {
+        word: "",
+        translation: "",
+        transcription: "",
+        rate: 1,
+        success: 0,
+        statistics: []
+    }, {
+        word: "",
+        translation: "",
+        transcription: "",
+        rate: 1,
+        success: 0,
+        statistics: []
+    }, {
+        word: "",
+        translation: "",
+        transcription: "",
+        rate: 1,
+        success: 0,
+        statistics: []
+    }, {
+        word: "",
+        translation: "",
+        transcription: "",
+        rate: 1,
+        success: 0,
+        statistics: []
+    }, {
+        word: "",
+        translation: "",
+        transcription: "",
+        rate: 1,
+        success: 0,
+        statistics: []
+    }, {
+        word: "",
+        translation: "",
+        transcription: "",
+        rate: 1,
+        success: 0,
+        statistics: []
+    }, {
+        word: "",
+        translation: "",
+        transcription: "",
+        rate: 1,
+        success: 0,
+        statistics: []
+    }, {
+        word: "",
+        translation: "",
+        transcription: "",
+        rate: 1,
+        success: 0,
+        statistics: []
+    }, {
+        word: "",
+        translation: "",
+        transcription: "",
+        rate: 1,
+        success: 0,
+        statistics: []
+    }, {
+        word: "",
+        translation: "",
+        transcription: "",
+        rate: 1,
+        success: 0,
+        statistics: []
+    }
+]
+
+export default wordsList
---
+++ trainer/scripts/index.js
@@ -0,0 +1,108 @@
+import wordsList from '../assets/db/dictionary.js'
+
+const appChoise = document.querySelector('#app-choice')
+const buttonsWrapper = document.querySelector('#buttons-wrapper')
+const prev = document.querySelector('#previous')
+const next = document.querySelector('#next')
+const learnForeign = document.querySelector('#learn-foreign')
+const learnTranslating = document.querySelector('#learn-translating')
+const practiceForeign = document.querySelector('#practice-foreign')
+const practiceTranslating = document.querySelector('#practice-translating')
+const numbersPrintedWords = []
+
+const randNumber = () => Math.floor(Math.random() * wordsList.length)
+
+const showItem = (item) => {
+    document.querySelector(`${item}`).classList.remove('js-hide')
+}
+
+const hideItem = (item) => {
+    document.querySelector(`${item}`).classList.add('js-hide')
+}
+
+const checkPrevButton = () => {
+    if (numbersPrintedWords.length > 1) {
+        prev.disabled = false
+    } else {
+        prev.disabled = true
+    }
+}
+
+const checkNextButton = () => {
+    if (practiceForeign.value.length > 0) {
+        next.disabled = false
+    } else {
+        next.disabled = true
+    }
+}
+
+const fillBlock = (block, number, method) => {
+    block.innerHTML = wordsList[number][method].toLowerCase()
+}
+
+const fillLearnBlocks = (wordNumber) => {
+    let number = 0
+    if (wordNumber) {
+        number = wordNumber
+    } else {
+        number = randNumber()
+    }
+    fillBlock(learnForeign, number, 'word')
+    fillBlock(learnTranslating, number, 'translation')
+    numbersPrintedWords.push(number)
+}
+
+const studying = () => {
+    showItem('#app-learning')
+    fillLearnBlocks()
+    next.addEventListener('click', () => {
+        fillLearnBlocks()
+        checkPrevButton()
+    })
+    prev.addEventListener('click', () => {
+        if (numbersPrintedWords.length > 1) {
+            numbersPrintedWords.length = numbersPrintedWords.length-1
+            fillLearnBlocks(numbersPrintedWords[numbersPrintedWords.length-1])
+            numbersPrintedWords.length = numbersPrintedWords.length-1
+            checkPrevButton()
+        }
+    })
+}
+
+const practicing = () => {
+    checkNextButton()
+    showItem('#app-practicing')
+    let number = randNumber()
+    practiceTranslating.innerHTML = wordsList[number].translation
+    practiceForeign.value = ''
+    practiceForeign.addEventListener('input',() => {
+        checkNextButton()
+    })
+    next.addEventListener('click', () => {
+        if (practiceForeign.value.trim() === wordsList[number].word) {
+            numbersPrintedWords.push(number)
+            number = randNumber()
+            practiceTranslating.innerHTML = wordsList[number].translation
+            practiceForeign.value = ''
+            checkPrevButton()
+        }
+        
+    })
+}
+
+const start = (mode) => {
+    hideItem('#app-choice')
+    showItem('#app-work')
+    if (mode === 'learn') {
+        studying()        
+    } else if (mode === 'practice') {
+        practicing()
+    }
+    buttonsWrapper.classList.remove('opacity-hide')
+    checkPrevButton()
+}
+
+appChoise.addEventListener('click', (event) => {
+    const mode = event.target.dataset.id
+    start(mode)
+})
